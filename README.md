980a852bc7090f439fb6927b28126a204ff4d8e3
rob <masterofblockchain20@gmail.com>
2022-09-16 18:29:53 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+hardhat self made starter kit
---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-ethers")
+module.exports = {
+    solidity: "0.8.7",
+}
