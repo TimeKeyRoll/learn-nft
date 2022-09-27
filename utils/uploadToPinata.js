35463ff242bf459d3cd5c4007d1f4024585b50a4
TimeKeyRoll <masterofblockchain20@gmail.com>
2022-09-27 13:03:08 +0530

    ok

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,11 +1,29 @@
 const pinataSDK = require("@pinata/sdk")
 const path = require("path")
 const fs = require("fs")
+require("dotenv").config()
+
+const pintaApikey = process.env.PINATA_API_KEY
+const pintaSecretkey = process.env.PINATA_SECRET_KEY
+const pinata = pinataSDK(pintaApikey, pintaSecretkey)
 
 async function storeImages(imagesFilePath) {
     const fullImagesPath = path.resolve(imagesFilePath)
     const files = fs.readdirSync(fullImagesPath)
-    conslo.log(files)
+    let responses = []
+    console.log("Uploading to IPFS")
+    for (fileIndex in files) {
+        const readableStreamForfile = fs.createReadStream(
+            `${fullImagesPath}/${files[fileIndex]}`
+        )
+        try {
+            const response = await pinata.pinFilesToIPFS(readableStreamForfile)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
 }
 
 module.exports = { storeImages }
